edccd082bf251733c411c1153cc40628d0a3cf16
Nastia Kuzina <miowch@gmail.com>
2025-04-13 12:41:48 +0200

    Implement location element logic

--- Task2/utils/ui/dashboard_page_object.py
+++ Task2/utils/ui/dashboard_page_object.py
@@ -1,31 +1,29 @@
 from typing import Final
 
-from appium.webdriver.common.appiumby import AppiumBy
-
 from Task2.utils.ui.main_page_object import MainPageObject
 
 
 class DashboardPageObject (MainPageObject):
-    expenseButton: Final = 'com.monefy.app.lite:id/expense_button'
-    sliderButton: Final = 'com.monefy.app.lite:id/leftLinesImageView'
-    amountText: Final = 'com.monefy.app.lite:id/amount_text'
-    noteInputField: Final = 'com.monefy.app.lite:id/textViewNote'
-    chooseCategoryButton: Final = 'com.monefy.app.lite:id/keyboard_action_button'
-    balanceAmount: Final = 'com.monefy.app.lite:id/balance_amount'
-    expenseAmount: Final = 'com.monefy.app.lite:id/expense_amount_text'
-    incomeAmount: Final = 'com.monefy.app.lite:id/income_amount_text'
+    expenseButton: Final = 'id:com.monefy.app.lite:id/expense_button'
+    sliderButton: Final = 'id:com.monefy.app.lite:id/leftLinesImageView'
+    amountText: Final = 'id:com.monefy.app.lite:id/amount_text'
+    noteInputField: Final = 'id:com.monefy.app.lite:id/textViewNote'
+    chooseCategoryButton: Final = 'id:com.monefy.app.lite:id/keyboard_action_button'
+    balanceAmount: Final = 'id:com.monefy.app.lite:id/balance_amount'
+    expenseAmount: Final = 'id:com.monefy.app.lite:id/expense_amount_text'
+    incomeAmount: Final = 'id:com.monefy.app.lite:id/income_amount_text'
 
-    keyboardButtonTmpl: str = 'com.monefy.app.lite:id/buttonKeyboard{char}'
-    categoryElementTmpl: str = '//android.widget.TextView[@resource-id="com.monefy.app.lite:id/textCategoryName" and @text="{text}"]'
-    textElementTmpl: str = '//android.widget.TextView[@text="{text}"]'
+    keyboardButtonTmpl: str = 'id:com.monefy.app.lite:id/buttonKeyboard{char}'
+    categoryElementTmpl: str = 'xpath://android.widget.TextView[@resource-id="com.monefy.app.lite:id/textCategoryName" and @text="{text}"]'
+    textElementTmpl: str = 'xpath://android.widget.TextView[@text="{text}"]'
 
     def add_new_expense(self, value, category, note=None):
         self.wait_for_element_and_click(
-            by=(AppiumBy.ID, self.expenseButton),
+            self.expenseButton,
             error_message='Cannot find expense_button'
         )
         self.wait_for_element_present(
-            by=(AppiumBy.ID, self.amountText),
+            self.amountText,
             error_message='Cannot find amount_text'
         )
 
@@ -38,37 +36,37 @@ class DashboardPageObject (MainPageObject):
             char = 'Divide' if char == '/' else char
 
             self.wait_for_element_and_click(
-                by=(AppiumBy.ID, self.keyboardButtonTmpl.replace('{char}', char)),
+                self.keyboardButtonTmpl.replace('{char}', char),
                 error_message=f'Cannot find buttonKeyboard{char}'
             )
 
         if note is not None:
             self.wait_for_element_and_send_keys(
-                by=(AppiumBy.ID, self.noteInputField),
+                self.noteInputField,
                 value=note,
                 error_message='Cannot find textViewNote'
             )
 
         self.wait_for_element_and_click(
-            by=(AppiumBy.ID, self.chooseCategoryButton),
+            self.chooseCategoryButton,
             error_message='Cannot find keyboard_action_button'
         )
 
         self.wait_for_element_and_click(
-            by=(AppiumBy.XPATH, self.categoryElementTmpl.replace('{text}', category)),
+            self.categoryElementTmpl.replace('{text}', category),
             error_message=f'Cannot find category {category}'
         )
 
     def assert_balance(self, expected_balance):
         self.assert_element_has_text(
-            by=(AppiumBy.ID, self.balanceAmount),
+            self.balanceAmount,
             expected_text=f'Balance {expected_balance}',
             error_message='Cannot find balance_amount'
         )
 
     def assert_percentage(self, percentage, occurrence_number=1):
         elements = self.wait_for_elements_present(
-            by=(AppiumBy.XPATH, self.textElementTmpl.replace('{text}', percentage)),
+            self.textElementTmpl.replace('{text}', percentage),
             error_message=f'Cannot find text {percentage}'
         )
 
@@ -78,19 +76,19 @@ class DashboardPageObject (MainPageObject):
         error_message='Cannot find text'
 
         self.assert_element_has_text(
-            by=(AppiumBy.ID, self.expenseAmount),
+            self.expenseAmount,
             expected_text=expense_amount,
             error_message=error_message
         )
 
         self.assert_element_has_text(
-            by=(AppiumBy.ID, self.incomeAmount),
+            self.incomeAmount,
             expected_text=income_amount,
             error_message=error_message
         )
 
     def open_balance_records(self):
         return self.wait_for_element_and_click(
-            by=(AppiumBy.ID, self.sliderButton),
+            self.sliderButton,
             error_message='Cannot find slider button'
         )
--- Task2/utils/ui/main_page_object.py
+++ Task2/utils/ui/main_page_object.py
@@ -1,47 +1,64 @@
+from appium.webdriver.common.appiumby import AppiumBy
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+
+def get_locator_by_string(locator_with_type):
+    exploded_locator = locator_with_type.split(":", 1)
+    locator_strategy = exploded_locator[0]
+    locator = exploded_locator[1]
+
+    if locator_strategy == "xpath":
+        return AppiumBy.XPATH, locator
+    elif locator_strategy == "id":
+        return AppiumBy.ID, locator
+    else:
+        raise ValueError("Cannot get type of locator. Locator: " + locator_strategy)
 
 
 class MainPageObject:
     def __init__(self, driver):
         self.driver = driver
 
-    def wait_for_element_present(self, by, error_message, timeout_in_sec=5):
+    def wait_for_element_present(self, locator, error_message, timeout_in_sec=5):
         wait = WebDriverWait(self.driver, timeout_in_sec)
+        by = get_locator_by_string(locator)
 
         return wait.until(
             EC.presence_of_element_located(by),
             message=error_message + '\n')
 
-    def wait_for_elements_present(self, by, error_message, timeout_in_sec=5):
+    def wait_for_elements_present(self, locator, error_message, timeout_in_sec=5):
         wait = WebDriverWait(self.driver, timeout_in_sec)
+        by = get_locator_by_string(locator)
 
         return wait.until(
             EC.presence_of_all_elements_located(by),
             message=error_message + '\n')
 
-    def wait_for_element_and_click(self, by, error_message, timeout_in_sec=5):
-        element = self.wait_for_element_present(by, error_message, timeout_in_sec)
+    def wait_for_element_and_click(self, locator, error_message, timeout_in_sec=5):
+        element = self.wait_for_element_present(locator, error_message, timeout_in_sec)
         element.click()
         return element
 
-    def wait_for_element_and_send_keys(self, by, value, error_message, timeout_in_sec=5):
-        element = self.wait_for_element_present(by, error_message, timeout_in_sec)
+    def wait_for_element_and_send_keys(self, locator, value, error_message, timeout_in_sec=5):
+        element = self.wait_for_element_present(locator, error_message, timeout_in_sec)
         element.send_keys(value)
         return element
 
-    def wait_for_element_not_present(self, by, error_message, timeout_in_sec=5):
+    def wait_for_element_not_present(self, locator, error_message, timeout_in_sec=5):
         wait = WebDriverWait(self.driver, timeout_in_sec)
+        by = get_locator_by_string(locator)
 
         return wait.until(
             EC.invisibility_of_element_located(by),
             message=error_message + '\n')
 
-    def wait_for_element_and_clear(self, by, error_message, timeout_in_sec=5):
-        element = self.wait_for_element_present(by, error_message, timeout_in_sec)
+    def wait_for_element_and_clear(self, locator, error_message, timeout_in_sec=5):
+        element = self.wait_for_element_present(locator, error_message, timeout_in_sec)
         element.clear()
         return element
 
-    def assert_element_has_text(self, by, expected_text, error_message):
-        element = self.wait_for_element_present(by, error_message)
+    def assert_element_has_text(self, locator, expected_text, error_message):
+        element = self.wait_for_element_present(locator, error_message)
         return element.text == expected_text
