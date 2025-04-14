13225913ad33874d72148fa9b6a9708a1a9edfd2
Nastia Kuzina <miowch@gmail.com>
2025-04-14 17:43:12 +0200

    Update test_add_pet.py

--- Task3/api-e2e-tests/tests/test_add_pet.py
+++ Task3/api-e2e-tests/tests/test_add_pet.py
@@ -1,14 +1,10 @@
+import random
 import pytest
 from lib.assertions import Assertions
 from lib.base_case import BaseCase
 from lib.my_requests import MyRequests
 
 class TestAddPet (BaseCase):
-    absent_required_fields = [
-         ("name"),
-         ("photoUrls")
-    ]
-
     def test_add_pet(self):
         failed_assertions = []
         data = self.prepare_pet_creation_data()
@@ -45,6 +41,10 @@ class TestAddPet (BaseCase):
         if failed_assertions:
             pytest.fail("Soft assertion failures:\n" + "\n".join(failed_assertions))
 
+    absent_required_fields = [
+         ("name"),
+         ("photoUrls")
+    ]
     @pytest.mark.parametrize("absent_required_field", absent_required_fields)
     def test_add_pet_without_required_field(self, absent_required_field):
         data = self.prepare_pet_creation_data()
@@ -71,3 +71,80 @@ class TestAddPet (BaseCase):
             data=new_pet_data2)
         
         Assertions.assert_code_status(response2, 400)
+
+    invalid_ids = [
+        ("15"),
+        ("two"),
+        ("2.5")
+    ]
+    @pytest.mark.parametrize("invalid_id", invalid_ids)
+    def test_add_pet_with_invalid_id(self, invalid_id):
+        data = {
+            "id": invalid_id,
+            "name": self.fake.first_name(),
+            "photoUrls": ["string"]
+        }
+
+        response = MyRequests.post(
+            "/pet/",
+            data)
+        
+        Assertions.assert_code_status(response, 405)
+
+    invalid_names = [
+        (400),
+        (True)
+    ]
+    @pytest.mark.parametrize("invalid_name", invalid_names)
+    def test_add_pet_with_invalid_name(self, invalid_name):
+        data = {
+            "id": random.randint(1, 1000),
+            "name": invalid_name,
+            "photoUrls": ["string"]
+        }
+
+        response = MyRequests.post(
+            "/pet/",
+            data)
+        
+        Assertions.assert_code_status(response, 405)
+
+    def test_add_pet_with_invalid_status(self):
+        data = {
+            "id": random.randint(1, 1000),
+            "name": self.fake.first_name(),
+            "photoUrls": ["string"],
+            "status": "wrong"
+        }
+
+        response = MyRequests.post(
+            "/pet/",
+            data)
+        
+        Assertions.assert_code_status(response, 405)
+
+    def test_add_pet_with_invalid_photoUrls(self):
+        data = {
+            "id": random.randint(1, 1000),
+            "name": self.fake.first_name(),
+            "photoUrls": ("string", 2, 7)
+        }
+
+        response = MyRequests.post(
+            "/pet/",
+            data)
+        
+        Assertions.assert_code_status(response, 405)
+
+    def test_add_pet_with_invalid_parameter(self):
+        data = {
+            "id": random.randint(1, 1000),
+            "name": self.fake.first_name(),
+            "owner": self.fake.name()
+        }
+
+        response = MyRequests.post(
+            "/pet/",
+            data)
+        
+        Assertions.assert_code_status(response, 405)
